993e29763dfe395169e3d06b5db19bfb358dedeb
juanantonio.lopezvisa@amaris.com <alvNa@users.noreply.github.com>
2024-09-05 02:09:00 +0200

    Clean code

--- src/main/java/com/alvna/neo4jdemo/services/MovieService.java
+++ src/main/java/com/alvna/neo4jdemo/services/MovieService.java
@@ -1,11 +1,21 @@
 package com.alvna.neo4jdemo.services;
 
 import com.alvna.neo4jdemo.repositories.MovieRepository;
+import com.alvna.neo4jdemo.repositories.PersonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
-    private final MovieRepository personRepository;
+    private final MovieRepository movieRepository;
+    private final PersonRepository personRepository;
+
+    public List<String> findPathBetweenActors(String actor1, String actor2){
+
+        return List.of();
+    }
+
 }
